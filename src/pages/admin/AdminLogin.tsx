223fcb23adaa9a0a07509ecbc5443ceca50b5191
Tusho7 <sandrotushurashvili7@gmail.com>
2024-07-09 09:39:57 +0400

    refactor: Update AdminLogin component to use Georgian language for labels and placeholders

--- src/pages/admin/AdminLogin.tsx
+++ src/pages/admin/AdminLogin.tsx
@@ -34,7 +34,7 @@ const AdminLogin = () => {
         {loading && <Loading />}
         <div className="px-6 py-8">
           <h2 className="text-center text-3xl font-extrabold text-gray-900">
-            Admin Login
+            შესვლა
           </h2>
 
           <form className="mt-6" onSubmit={handleSubmit}>
@@ -43,7 +43,7 @@ const AdminLogin = () => {
                 htmlFor="username"
                 className="block text-sm font-medium text-gray-700"
               >
-                Username
+                ელ-ფოსტა
               </label>
               <input
                 id="username"
@@ -53,7 +53,7 @@ const AdminLogin = () => {
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Enter your username"
+                placeholder="შეიყვანეთ ელ-ფოსტა"
               />
             </div>
             <div className="mt-4">
@@ -61,7 +61,7 @@ const AdminLogin = () => {
                 htmlFor="password"
                 className="block text-sm font-medium text-gray-700"
               >
-                Password
+                პაროლი
               </label>
               <input
                 id="password"
@@ -71,7 +71,7 @@ const AdminLogin = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Enter your password"
+                placeholder="შეიყვანეთ პაროლი"
               />
             </div>
             <div className="mt-6">
@@ -79,18 +79,18 @@ const AdminLogin = () => {
                 type="submit"
                 className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Sign In
+                შესვლა
               </button>
             </div>
           </form>
-          <div className="mt-6 text-center">
+          {/* <div className="mt-6 text-center">
             <a
               href="/"
               className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
             >
               Forgot your password?
             </a>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
